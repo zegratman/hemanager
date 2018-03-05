23b76e1b01ab4e7e50301e9180e18ce54adf0037
zegratman <kobaal25**>
2018-03-05 21:55:36 +0100

    tutorial niveau 7

--- python-django-test/mysite/polls/admin.py
+++ python-django-test/mysite/polls/admin.py
@@ -2,9 +2,25 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.utils.translation import ugettext as ugettext_lazy
 
 # Register your models here.
 
-from models import Question
+from models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.site_header = ugettext_lazy('django-test administration')
+admin.site.register(Question, QuestionAdmin)
--- python-django-test/mysite/polls/models.py
+++ python-django-test/mysite/polls/models.py
@@ -6,6 +6,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
+
 @python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
