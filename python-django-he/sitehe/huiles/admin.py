0524507384c88a4b5c8f51454caf118eac49dc03
zegratman <kobaal25**>
2018-03-08 22:53:03 +0100

    starting huile website !

---
+++ python-django-he/sitehe/huiles/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from models import HuileEssentielle
+
+# Register your models here.
+
+### TODO register huile famille !!!
+
+admin.site.register(HuileEssentielle)
