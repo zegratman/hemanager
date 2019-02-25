3d95ebe0f232df42589d4a425a75966013a83ed3
zegratman <kobaal25**>
2019-02-25 23:11:47 +0100

    add primary and secondary faimilies in a table for standard he list

--- python-django-he/sitehe/huiles/static/huiles/hemanager.js
+++ python-django-he/sitehe/huiles/static/huiles/hemanager.js
@@ -10,7 +10,21 @@ function(){
   }
 
   // Main accordion
-  $("#he-list").accordion();
+  $("#he-list").accordion({
+      heightStyle: "content"
+  });
+
+  // Tableaux des familles
+  $('.famille-display').DataTable({
+        "paging":   false,
+        "ordering": false,
+        "info":     false,
+        "searching": false,
+        "columnDefs": [
+            { className: "dt-body-center", "targets": [ 0, 1, 2 ] }
+        ]
+  });
+  $('.dataTables_wrapper').css("width","600px")
 
   // Filtering - Contre Indication menu
   $("#contre_indic").selectmenu();
