c94231b2ab400d607d9edfc4f0f56e6703cc3798
zegratman <zegratman@gmail.com>
2019-11-23 15:34:45 +0100

    some refactoring for cleaner HE filtering

--- python-django-he/sitehe/huiles/views.py
+++ python-django-he/sitehe/huiles/views.py
@@ -1,15 +1,69 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import urllib
+
 from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 
 from models import HuileEssentielle, ContreIndication, ContreIndicationHE, ProprieteEffective, Propriete, Famille
 
-import urllib
-
 
 # Create your views here.
+
+
+def remove_ci_from(filtering, filtered_list):
+    """
+    Remove the HE using filtering based on CI
+    :param filtering: the filtering
+    :param filtered_list:  the current HE list
+    :return: the updated filtered list
+    """
+    if 'contre_indic' in filtering and filtering.get('contre_indic') != 'none':
+        to_be_excluded_list = ContreIndicationHE.objects.filter(
+            nom_contre_indication__in=filtering.getlist('contre_indic'))
+        for excluded in to_be_excluded_list:
+            if excluded.nom_he_id in filtered_list:
+                filtered_list.remove(excluded.nom_he_id)
+    return filtered_list
+
+
+def remove_props_from(filtering, filtered_list):
+    """
+    Remove the HEs using filtering based on properties
+    :param filtering: the filtering
+    :param filtered_list: the current HE list
+    :return: the updated filtered list
+    """
+    if 'property' in filtering and filtering.get('property') != 'none':
+        filtered_list = [he for he in filtered_list if
+                         len(ProprieteEffective.objects.filter(nom_he__exact=he).filter(
+                             nom_prop__exact=filtering.get('property'))) > 0]
+    return filtered_list
+
+
+def remove_families_from(filtering, filtered_list):
+    """
+    Remove the HEs using filtering based on families
+    :param filtering: the filtering
+    :param filtered_list: the current HE list
+    :return: the updated filtered list
+    """
+    if 'family' in filtering and filtering.get('family') != 'none':
+        to_be_excluded_list = []
+        families_attr = ['famille_{0}'.format(i) for i in range(1, 6)]
+        for he_nom in filtered_list:
+            he = HuileEssentielle.objects.get(pk=he_nom)
+            families = []
+            for attr in families_attr:
+                he_famille = getattr(he, attr)
+                if he_famille is not None:
+                    families.append(he_famille.nom)
+            if filtering.get('family') not in families:
+                to_be_excluded_list.append(he_nom)
+        for excluded in to_be_excluded_list:
+            filtered_list.remove(excluded)
+    return filtered_list
 
 
 def he_view(request):
@@ -25,7 +79,6 @@ def he_view(request):
     ci_list = ContreIndication.objects.all()
     prop_list = Propriete.objects.all()
     famille_list = Famille.objects.all()
-    ci_he_list = None
 
     # Effective properties
     eff_props = dict()
@@ -38,35 +91,13 @@ def he_view(request):
     if filtering:
 
         # CI filtering
-        if 'contre_indic' in filtering and filtering.get('contre_indic') != 'none':
-            to_be_excluded_list = ContreIndicationHE.objects.filter(
-                nom_contre_indication__in=filtering.getlist('contre_indic'))
-            for excluded in to_be_excluded_list:
-                if excluded.nom_he_id in filtered_list:
-                    filtered_list.remove(excluded.nom_he_id)
+        filtered_list = remove_ci_from(filtering, filtered_list)
 
         # Property filtering
-        if 'property' in filtering and filtering.get('property') != 'none':
-            filtered_list = [he for he in filtered_list if
-                             len(ProprieteEffective.objects.filter(nom_he__exact=he).filter(
-                                 nom_prop__exact=filtering.get('property'))) > 0]
+        filtered_list = remove_props_from(filtering, filtered_list)
 
         # Family filtering
-        if 'family' in filtering and filtering.get('family') != 'none':
-            to_be_excluded_list = []
-            families_attr = ['famille_{0}'.format(i) for i in range(1, 6)]
-            for he_nom in filtered_list:
-                he = HuileEssentielle.objects.get(pk=he_nom)
-                families = []
-                for attr in families_attr:
-                    he_famille = getattr(he, attr)
-                    if he_famille is not None:
-                        families.append(he_famille.nom)
-                if filtering.get('family') not in families:
-                    to_be_excluded_list.append(he_nom)
-            for excluded in to_be_excluded_list:
-                filtered_list.remove(excluded)
-
+        filtered_list = remove_families_from(filtering, filtered_list)
 
     # context in return
     context = {'he_list': he_list, 'ci_list': ci_list, 'prop_list': prop_list, 'family_list': famille_list,
